70df0432bf6a753a9a0f2c5edb2bf1872e082e7d
Dmytro-Hladkykh <gladkikhd68@gmail.com>
2025-05-27 18:30:14 +0300

    Fix resource

--- resources/model_state_v2_attributes.go
+++ resources/model_state_v2_attributes.go
@@ -6,7 +6,7 @@ package resources
 
 type StateV2Attributes struct {
 	// Root hash of the state tree
-	Root string `json:"root,omitempty"`
+	Root string `json:"root"`
 	// Signature of root state signed by relayer private key, with the last byte of the signature set to 27/28.
 	Signature string `json:"signature"`
 	// Time indicates when the event was caught, a.k.a state transition timestamp
